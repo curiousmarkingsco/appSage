3911017276ad4c96bd854d05a2e95dcd2f42f0cd
ianrandmckenzie <ian.rand.mckenzie@gmail.com>
2024-07-17 11:54:27 -0700

    Add conditional for blank pages

--- js/load.js
+++ js/load.js
@@ -57,15 +57,17 @@ function loadChanges(json) {
   });
 
   const grid = document.querySelector('#page .grid');
-  const pageElement = document.getElementById('page');
-  const gridCount = Array.prototype.slice.call(pageElement.children).length;
-  const buttonBar = document.getElementById('gridButtonsBottombar')
-  if (gridCount > 1) {
-    document.querySelectorAll('.moveGrid').forEach(element => element.remove());
-    buttonBar.prepend(createVerticalMoveGridButton(grid, 'up'));
-    buttonBar.appendChild(createVerticalMoveGridButton(grid, 'down'));
+  if (grid) {
+    const pageElement = document.getElementById('page');
+    const gridCount = Array.prototype.slice.call(pageElement.children).length;
+    const buttonBar = document.getElementById('gridButtonsBottombar')
+    if (gridCount > 1) {
+      document.querySelectorAll('.moveGrid').forEach(element => element.remove());
+      buttonBar.prepend(createVerticalMoveGridButton(grid, 'up'));
+      buttonBar.appendChild(createVerticalMoveGridButton(grid, 'down'));
+    }
+    addGridOptions(grid);
   }
-  addGridOptions(grid);
 }
 
 // Utility functions for managing localStorage with a 'tailwindvpb' object
