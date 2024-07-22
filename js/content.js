0dad590e5a16e38685b52bee2d48aaf15160d54e
Ian McKenzie <admin@ianrandmckenzie.com>
2024-07-22 10:48:55 -0700

    Finish adding tooltip messages to icon buttons

--- js/content.js
+++ js/content.js
@@ -36,7 +36,8 @@ function createAddContentButton(contentContainer) {
 
 function createRemoveContentButton(contentContainer) {
   const button = document.createElement('button');
-  button.className = 'removeContent ugc-discard bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded h-12 w-12';
+  button.setAttribute('data-extra-info', 'Remove this content forever (that\'s a long time!')
+  button.className = 'removeContent ugc-discard bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded h-12 w-12 mx-auto';
   button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="white" class="h-5 w-5 mx-auto"><!--!Font Awesome Pro 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L409.6 295.8l42.1-42.1L474.3 231l11.3-11.3-33.9-33.9-62.1-62.1L355.7 89.8l-11.3 11.3-22.6 22.6-57.8 57.8L38.8 5.1zM306.2 214.7l50.5-50.5c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6l-47.8 47.8-25.4-19.9zM195.5 250l-72.9 72.9c-10.4 10.4-18 23.3-22.2 37.4L65 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2l88.3-88.3-77.9-61.4-27.6 27.6c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l24.9-24.9L195.5 250zM224 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9l-78.1 23 23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM426.7 18.7L412.3 33.2 389.7 55.8 378.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L517.3 18.7c-25-25-65.5-25-90.5 0z"/></svg>';
   button.addEventListener('click', function () {
     showConfirmationModal('Are you sure you want to delete this content?', () => {
@@ -49,6 +50,7 @@ function createRemoveContentButton(contentContainer) {
 
 function createVerticalMoveContentButton(contentContainer, direction) {
   const button = document.createElement('button');
+  button.setAttribute('data-extra-info', `Move this content ${direction}ward in the column`)
   button.className = 'moveContent ugc-discard bg-yellow-500 hover:bg-yellow-700 text-white font-bold p-2 rounded h-12 w-16';
   if (direction == 'up') {
     button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" class="h-4 w-4 inline"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>';
@@ -96,9 +98,12 @@ function detectAndLoadContentType(contentContainer) {
   moveButtons.className = 'flex justify-between my-2';
   moveButtons.id = 'moveContentButtons';
   sidebar.prepend(moveButtons);
-  moveButtons.appendChild(createVerticalMoveContentButton(contentContainer, 'up'));
+
+  // Minus one to remove the 'Add Content' button from the count
+  const contentCount = contentContainer.parentElement.children.length - 1
+  if (contentCount > 1) moveButtons.appendChild(createVerticalMoveContentButton(contentContainer, 'up'));
   moveButtons.appendChild(createRemoveContentButton(contentContainer));
-  moveButtons.appendChild(createVerticalMoveContentButton(contentContainer, 'down'));
+  if (contentCount > 1) moveButtons.appendChild(createVerticalMoveContentButton(contentContainer, 'down'));
 
   highlightEditingElement(contentContainer);
 }
--- js/grid.js
+++ js/grid.js
@@ -10,9 +10,11 @@ function addGridOptions(grid) {
     moveButtons.className = 'flex justify-between my-2'
     moveButtons.id = 'moveGridButtons'
     sidebar.prepend(moveButtons);
-    moveButtons.appendChild(createVerticalMoveGridButton(grid, 'up'));
+
+    const gridCount = document.getElementById('page').querySelectorAll('.grid').length
+    if (gridCount > 1) moveButtons.appendChild(createVerticalMoveGridButton(grid, 'up'));
     moveButtons.appendChild(addRemoveGridButton(grid, sidebar));
-    moveButtons.appendChild(createVerticalMoveGridButton(grid, 'down'));
+    if (gridCount > 1) moveButtons.appendChild(createVerticalMoveGridButton(grid, 'down'));
 
     addEditableColumns(sidebar, grid);
     addGridAlignmentOptions(sidebar, grid)
@@ -28,7 +30,8 @@ function addGridOptions(grid) {
 
 function addRemoveGridButton(grid, sidebar) {
   const button = document.createElement('button');
-  button.className = 'bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded h-12 w-12';
+  button.setAttribute('data-extra-info', 'Remove this grid forever (that\'s a long time!')
+  button.className = 'removeGrid bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded h-12 w-12 mx-auto';
   button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="white" class="h-5 w-5 mx-auto"><!--!Font Awesome Pro 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.--><path d="M0 64C0 46.3 14.3 32 32 32l512 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64zm32 64l512 0L517.3 421.8c-3 33-30.6 58.2-63.7 58.2l-331.1 0c-33.1 0-60.7-25.2-63.7-58.2L32 128zm256 88c2.2 0 4.3 1.1 5.5 2.9l20.7 29.6c7.3 10.5 21.6 13.4 32.4 6.6c11.7-7.3 14.8-22.9 6.9-34.1l-20.7-29.6c-10.2-14.6-27-23.3-44.8-23.3s-34.6 8.7-44.8 23.3l-20.7 29.6c-7.9 11.3-4.7 26.8 6.9 34.1c10.8 6.8 25.1 3.9 32.4-6.6l20.7-29.6c1.3-1.8 3.3-2.9 5.5-2.9zm-88.3 77.1c-10.8-6.8-25.1-3.9-32.4 6.6l-21.5 30.7c-6.4 9.1-9.8 20-9.8 31.2c0 30.1 24.4 54.4 54.4 54.4l49.6 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-49.6 0c-3.5 0-6.4-2.9-6.4-6.4c0-1.3 .4-2.6 1.2-3.7l21.5-30.7c7.9-11.3 4.7-26.8-6.9-34.1zM312 392c0 13.3 10.7 24 24 24l49.6 0c30.1 0 54.4-24.4 54.4-54.4c0-11.2-3.4-22.1-9.8-31.2l-21.5-30.7c-7.3-10.5-21.6-13.4-32.4-6.6c-11.7 7.3-14.8 22.9-6.9 34.1l21.5 30.7c.8 1.1 1.2 2.4 1.2 3.7c0 3.5-2.9 6.4-6.4 6.4L336 368c-13.3 0-24 10.7-24 24z"/></svg>';
   button.onclick = function () {
     showConfirmationModal('Are you sure you want to delete this entire grid?', () => {
@@ -53,6 +56,7 @@ function setGridWidth(grid, widthClasses, plainEnglish) {
 
 function createVerticalMoveGridButton(grid, direction) {
   const button = document.createElement('button');
+  button.setAttribute('data-extra-info', `Move this grid ${direction}ward in the document`)
   button.className = 'moveGrid inline ugc-discard bg-yellow-500 hover:bg-yellow-700 text-white font-bold pt-1 pb-1.5 rounded w-12';
   if (direction == 'up') {
     button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" class="h-4 w-4 inline"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>';
