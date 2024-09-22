16ac4258846e11db7294381bec0766fb26c156b1
Ian McKenzie <admin@ianrandmckenzie.com>
2024-09-22 15:30:59 -0700

    Include css for /dist

--- generate_js_with_tw.sh
+++ generate_js_with_tw.sh
@@ -5,11 +5,14 @@ echo "Creating dist directory..."
 mkdir -p ./dist
 
 # Output file
-output_file="./dist/appSage.tailwind.js"
+output_js="./dist/appSage.tailwind.js"
+output_css="./dist/appSage.css"
+css_file="./app/css/main.css"
 
 # Clear the output file if it exists
-echo "Clearing the output file..."
-echo -n "" > "$output_file"
+echo "Clearing the output files..."
+echo -n "" > "$output_js"
+echo -n "" > "$output_css"
 
 # Define an array of the required files in order
 declare -a js_files=(
@@ -45,13 +48,18 @@ for js_file in "${js_files[@]}"
 do
     echo "Processing $js_file..."
     if [[ -f "$js_file" ]]; then
-        echo "Appending $js_file to $output_file"
-        echo "/* File: $js_file */" >> "$output_file"
-        cat "$js_file" >> "$output_file"
-        echo -e "\n" >> "$output_file"  # Add a newline for separation
+        echo "Appending $js_file to $output_js"
+        echo "/* File: $js_file */" >> "$output_js"
+        cat "$js_file" >> "$output_js"
+        echo -e "\n" >> "$output_js"  # Add a newline for separation
     else
         echo "Warning: $js_file not found, skipping."
     fi
 done
 
-echo "All specified JS files have been merged into $output_file"
+echo "Appending $css_file to $output_css"
+echo "/* File: $css_file */" >> "$output_css"
+cat "$css_file" >> "$output_css"
+echo -e "\n" >> "$output_css"  # Add a newline for separation
+
+echo "All specified JS files have been merged into $output_js and $output_css"
