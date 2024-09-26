76fe1764d74f0e355517daba829ea14518944ce8
Ian McKenzie <admin@ianrandmckenzie.com>
2024-09-26 14:26:55 -0700

    Make storage object name customizable for 3rd-party applications

--- app/js/main.js
+++ app/js/main.js
@@ -40,9 +40,9 @@ function deletePage(page_id, element) {
   const message = "Are you sure you want to delete this page? This action cannot be undone.";
 
   showConfirmationModal(message, function() {
-    const appSageStorage = JSON.parse(localStorage.getItem('appSageStorage'));
+    const appSageStorage = JSON.parse(localStorage.getItem(appSageStorageString));
     delete appSageStorage.pages[page_id];
-    localStorage.setItem('appSageStorage', JSON.stringify(appSageStorage));
+    localStorage.setItem(appSageStorageString, JSON.stringify(appSageStorage));
     element.remove();
   });
 } // DATA OUT: null
