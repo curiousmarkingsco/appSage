ab05ecc98c034cb282655d3fc91d33cbc6c2b506
Ian McKenzie <admin@ianrandmckenzie.com>
2024-10-10 07:12:54 -0700

    Reduce visual clutter while editing

--- app/js/editor/container.js
+++ app/js/editor/container.js
@@ -49,7 +49,7 @@ function addContainerOptions(container) {
 function createAddContainerButton(containingBox) {
   const button = document.createElement('button');
   button.setAttribute('data-extra-info', tooltips['add-container']);
-  button.className = 'addContainer hidden w-48 h-12 ugc-discard bg-sky-500 hover:bg-sky-700 text-slate-50 font-bold p-2 rounded';
+  button.className = 'addContainer highlightButton hidden w-16 h-12 absolute -bottom-12 left-0 ugc-discard bg-sky-500 hover:bg-sky-700 text-slate-50 font-bold p-2 rounded-bl z-50';
   button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" class="h-4 w-4 inline"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" /></svg><svg class="w-4 h-4 inline" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M48 32C21.5 32 0 53.5 0 80L0 240c0 26.5 21.5 48 48 48l96 0c26.5 0 48-21.5 48-48l0-160c0-26.5-21.5-48-48-48L48 32zM304 224c-26.5 0-48 21.5-48 48l0 160c0 26.5 21.5 48 48 48l96 0c26.5 0 48-21.5 48-48l0-160c0-26.5-21.5-48-48-48l-96 0zM0 400l0 32c0 26.5 21.5 48 48 48l96 0c26.5 0 48-21.5 48-48l0-32c0-26.5-21.5-48-48-48l-96 0c-26.5 0-48 21.5-48 48zM304 32c-26.5 0-48 21.5-48 48l0 32c0 26.5 21.5 48 48 48l96 0c26.5 0 48-21.5 48-48l0-32c0-26.5-21.5-48-48-48l-96 0z"/></svg>`;
   button.addEventListener('click', function () {
     const containerContainer = document.createElement('div');
@@ -71,16 +71,17 @@ function createAddContainerButton(containingBox) {
 
     enableEditContainerOnClick(containerContainer);
   });
-  containingBox.addEventListener('mouseover', function(event){
-    event.stopPropagation();
-    button.classList.add('block');
-    button.classList.remove('hidden');
-  });
-  containingBox.addEventListener('mouseout', function(event){
-    event.stopPropagation();
-    button.classList.add('hidden');
-    button.classList.remove('block');
-  });
+  // This creates a reliable hover effect for many nested elements
+  // containingBox.addEventListener('mouseover', function(event){
+  //   event.stopPropagation();
+  //   button.classList.add('block');
+  //   button.classList.remove('hidden');
+  // });
+  // containingBox.addEventListener('mouseout', function(event){
+  //   event.stopPropagation();
+  //   button.classList.add('hidden');
+  //   button.classList.remove('block');
+  // });
   return button;
 } 
 
