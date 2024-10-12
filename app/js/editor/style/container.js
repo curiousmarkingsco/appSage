f7a7fcefd1ef1bfad6737b31769423185b75c84a
Ian McKenzie <admin@ianrandmckenzie.com>
2024-10-11 23:39:52 -0700

    Add missing tooltips

--- app/js/editor/style/container.js
+++ app/js/editor/style/container.js
@@ -14,13 +14,12 @@
 // DATA IN: ['HTML Element, <div id="sidebar-dynamic">', 'HTML Element, <div>']
 function addContainerAlignmentOptions(sidebar, container) {
   const justifyContentOptions = ['start', 'end', 'center', 'stretch', 'between', 'around', 'evenly', 'reset'];
-  const alignItemsOptions = ['start', 'end', 'center', 'baseline', 'stretch', 'reset'];
-  const alignSelfOptions = ['start', 'end', 'center', 'stretch', 'reset'];
+  const alignOptions = ['start', 'end', 'center', 'stretch', 'reset'];
 
   // Justify Content - See: https://tailwindcss.com/docs/justify-content
   addDeviceTargetedOptions(sidebar, container, 'Justify Content', 'justify', justifyContentOptions, 'icon-select');
   // Align Items - See: https://tailwindcss.com/docs/align-items
-  addDeviceTargetedOptions(sidebar, container, 'Align Items', 'items', alignItemsOptions, 'icon-select');
+  addDeviceTargetedOptions(sidebar, container, 'Align Items', 'items', alignOptions, 'icon-select');
   // Align Self - See: https://tailwindcss.com/docs/place-items
-  addDeviceTargetedOptions(sidebar, container, 'Align Self', 'self', alignSelfOptions, 'icon-select');
+  addDeviceTargetedOptions(sidebar, container, 'Align Self', 'self', alignOptions, 'icon-select');
 } // DATA OUT: null
